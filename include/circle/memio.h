7593dee6a6614a3f9f2591485d7a347d268c41bf
Rene Stange <rsta2@gmx.net>
2025-11-25 23:24:54 +0100

    memio: This implementation is atomic even with strict-align
    Some modules, which are accessing coherent memory, have to be compiled
    with the -fstrict-align option. With this the previous implementation
    of the MMIO function did still not work reliable. Fix it using builtin
    GCC functions.

--- include/circle/memio.h
+++ include/circle/memio.h
@@ -22,6 +22,8 @@
 
 #include <circle/types.h>
 
+#define MEM_ORDER	__ATOMIC_RELAXED
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -29,37 +31,37 @@ extern "C" {
 /// \brief Read 8-bit value from MMIO address
 static inline u8 read8 (uintptr ulAddress)
 {
-	return *(volatile u8 *) ulAddress;
+	return __atomic_load_n ((volatile u8 *) ulAddress, MEM_ORDER);
 }
 
 /// \brief Write 8-bit value to MMIO address
 static inline void write8 (uintptr ulAddress, u8 uchValue)
 {
-	*(volatile u8 *) ulAddress = uchValue;
+	__atomic_store_n ((volatile u8 *) ulAddress, uchValue, MEM_ORDER);
 }
 
 /// \brief Read 16-bit value from MMIO address
 static inline u16 read16 (uintptr ulAddress)
 {
-	return *(volatile u16 *) ulAddress;
+	return __atomic_load_n ((volatile u16 *) ulAddress, MEM_ORDER);
 }
 
 /// \brief Write 16-bit value to MMIO address
 static inline void write16 (uintptr ulAddress, u16 usValue)
 {
-	*(volatile u16 *) ulAddress = usValue;
+	__atomic_store_n ((volatile u16 *) ulAddress, usValue, MEM_ORDER);
 }
 
 /// \brief Read 32-bit value from MMIO address
 static inline u32 read32 (uintptr ulAddress)
 {
-	return *(volatile u32 *) ulAddress;
+	return __atomic_load_n ((volatile u32 *) ulAddress, MEM_ORDER);
 }
 
 /// \brief Write 32-bit value to MMIO address
 static inline void write32 (uintptr ulAddress, u32 nValue)
 {
-	*(volatile u32 *) ulAddress = nValue;
+	__atomic_store_n ((volatile u32 *) ulAddress, nValue, MEM_ORDER);
 }
 
 #if AARCH == 64
@@ -67,13 +69,13 @@ static inline void write32 (uintptr ulAddress, u32 nValue)
 /// \brief Read 64-bit value from MMIO address
 static inline u64 read64 (uintptr ulAddress)
 {
-	return *(volatile u64 *) ulAddress;
+	return __atomic_load_n ((volatile u64 *) ulAddress, MEM_ORDER);
 }
 
 /// \brief Write 64-bit value to MMIO address
-static inline void write64 (uintptr ulAddress, u64 nValue)
+static inline void write64 (uintptr ulAddress, u64 ulValue)
 {
-	*(volatile u64 *) ulAddress = nValue;
+	__atomic_store_n ((volatile u64 *) ulAddress, ulValue, MEM_ORDER);
 }
 
 #endif
